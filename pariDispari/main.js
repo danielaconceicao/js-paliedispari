189e15c276cba4c7c355a096e62311364b5a4410
danielaconceicao <daniela.3597@gmail.com>
2024-10-04 17:12:21 +0200

    added 'Number' in the variable 'userNumber'

--- pariDispari/main.js
+++ pariDispari/main.js
@@ -3,10 +3,10 @@ Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
  */
 
-let userNumber = 4; /* prompt('inserici un numero da 1 a 5') */; 
+let userNumber = Number(prompt('inserici un numero da 1 a 5'));
 
 function generateRandomNumber() {
-    return randomNumber = parseInt(Math.random() * 5 + 1);
+    return randomNumber = Math.floor(Math.random() * 5) + 1;
 }
 
 let sun = userNumber + generateRandomNumber();
@@ -18,7 +18,5 @@ function evenOrOdd() {
         return `Il numero ${sun} è dispari`;
     }
 } 
-
-/* console.log(generateRandomNumber()); */
 
 console.log(evenOrOdd())
